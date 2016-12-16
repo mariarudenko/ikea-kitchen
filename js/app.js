9ccec27d53de47532750b09cbdd9d71b8e799c0b
Maria Rudenko <withering.vortex@gmail.com>
2016-12-16 04:00:09 +0400

    menu

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// $(window).scroll(function() {
+// if ($(this).scrollTop() > 1){
+// $('k-page__menu').addClass("sticky");
+// }
+// else{
+// $('k-page__menu').removeClass("sticky");
+// }
+// });
