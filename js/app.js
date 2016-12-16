e63fa1245ac5467ef0e5770cf72a52dc2d0cfad7
Maria Rudenko <withering.vortex@gmail.com>
2016-12-16 21:24:06 +0400

    for github page

--- js/app.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-  $("#pink-glass").click(function(){
-    $("#glass-color").text("розовый");
-    $("#glass-image").html('<img class="k-main__img-glass" src="images/pink.png" width="164" height="164">');
-  });
