b4db034264f4ff3eef942ae599a83774604f6e7a
Maria Rudenko <withering.vortex@gmail.com>
2016-12-16 21:19:52 +0400

    carousel

--- js/app.js
+++ js/app.js
@@ -1,8 +1,5 @@
-// $(window).scroll(function() {
-// if ($(this).scrollTop() > 1){
-// $('k-page__menu').addClass("sticky");
-// }
-// else{
-// $('k-page__menu').removeClass("sticky");
-// }
-// });
+$(document).ready(function(){
+  $("#pink-glass").click(function(){
+    $("#glass-color").text("розовый");
+    $("#glass-image").html('<img class="k-main__img-glass" src="images/pink.png" width="164" height="164">');
+  });
